c7daaa229513eb8cc992e3cc4a06bee23e934248
Raymond Chenon <raymond.chenon@gmail.com>
2017-06-17 16:46:10 +0200

    add dependencies

--- app/src/main/java/com/raychenon/distancecalculation/FormActivity.java
+++ app/src/main/java/com/raychenon/distancecalculation/FormActivity.java
@@ -1,12 +1,23 @@
 package com.raychenon.distancecalculation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.widget.AutoCompleteTextView;
+
+import butterknife.BindView;
 
 public class FormActivity extends AppCompatActivity {
 
+    @BindView(R.id.start_auto_textview)
+    AutoCompleteTextView startAutoTxtView;
+
+    @BindView(R.id.end_auto_textview)
+    AutoCompleteTextView endAutoTxtView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form_activity);
     }
---
+++ app/src/main/java/com/raychenon/distancecalculation/params/Constants.java
@@ -0,0 +1,6 @@
+package com.raychenon.distancecalculation.params;
+
+public class Constants {
+
+    public static final String GOOGLE_API_KEY = "AIzaSyC1ZsVlRnNhu7cNBuZwV-6CErmRBlxnkUE";
+}
